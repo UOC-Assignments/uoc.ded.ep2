62108cbad8e8e06eec561aebccbd05b36ae1db88
jbericat <jbericat@uoc.edu>
2020-11-16 02:55:51 +0100

    v3.3

--- src/main/java/uoc/ded/practica/model/Question.java
+++ src/main/java/uoc/ded/practica/model/Question.java
@@ -1,5 +1,8 @@
 package uoc.ded.practica.model;
 
+import uoc.ded.practica.Trial4C19;
+import uoc.ded.practica.Trial4C19.Type;
+
 /* Segons la solució de la PAC1, per a emmagatzemar les preguntes dins  
  * d'un grup de preguntes s'ha d'utilitzar una estructura de "llista 
  * encadenada" per a representar el  TAD "Question":
@@ -17,14 +20,52 @@ package uoc.ded.practica.model;
  * 
  */
 
-import uoc.ei.tads.LlistaEncadenada;
-
 public class Question {
-
-	public Object getIdQuestion() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	private String idQuestion;
+	private String wording;
+	private Trial4C19.Type type;
+	private String[] choices;
+	
+	public Question(String idQuestion, String wording, Type type, String[] choices) {
+		this.setIdQuestion(idQuestion);
+		this.setWording(wording);
+		this.setType(type);
+		this.setChoices(choices);
 	}
+
+	public String getIdQuestion() {
+		return idQuestion;
+	}
+
+	public void setIdQuestion(String idQuestion) {
+		this.idQuestion = idQuestion;
+	}
+
+	public String getWording() {
+		return wording;
+	}
+
+	public void setWording(String wording) {
+		this.wording = wording;
+	}
+
+	public Trial4C19.Type getType() {
+		return type;
+	}
+
+	public void setType(Trial4C19.Type type) {
+		this.type = type;
+	}
+
+	public String[] getChoices() {
+		return choices;
+	}
+
+	public void setChoices(String[] choices) {
+		this.choices = choices;
+	}
+
 	
 	//TO-DO
 
